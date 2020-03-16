9575c9548b7cea1c48e85a4ffcd117dcf3a66470
Andrew Palmer <palmer.a.w@gmail.com>
2020-03-16 22:14:43 +0100

    Update comments

--- include/NormalRandomVariable.h
+++ include/NormalRandomVariable.h
@@ -3,14 +3,19 @@
 namespace NRV {
 
 /**
- * Class that implements an independent normal random variable
+ * Class that implements an independent normal random variable and various operations
  */
 class NormalRandomVariable {
 public:
     /**
-     * Constructors
+     * Constructor for a NRV with a standard normal distribution
      */
     NormalRandomVariable();
+
+    /**
+     * Constructor for a NRV with specified mean and variance
+     * Note: Will throw an exception if variance is not greater than 0
+     */
     NormalRandomVariable(double mean, double variance);
 
 private:
--- src/NormalRandomVariable.cpp
+++ src/NormalRandomVariable.cpp
@@ -15,7 +15,7 @@ NormalRandomVariable::NormalRandomVariable(double mean, double variance)
 : mean_(mean), variance_(variance)
 {
     if(variance_ <= 0){
-        throw std::range_error("NormalRandomVariable: Variance must be positive");
+        throw std::range_error("NormalRandomVariable: Variance must be greater than 0");
     }
 }
 
