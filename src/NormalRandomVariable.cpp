d69e729074975a8715d4b7454b6c622905357a6c
Andrew Palmer <palmer.a.w@gmail.com>
2020-03-26 21:27:33 +0100

    Remove sqrt and pow where possible

--- src/NormalRandomVariable.cpp
+++ src/NormalRandomVariable.cpp
@@ -33,13 +33,14 @@ double NormalRandomVariable::variance() const
 NormalRandomVariable NormalRandomVariable::inverse() const
 {
     // This approximation is breaks down if the distribution is too close to 0. Set an arbitrary limit of 4 sigma. 
-    if(mean_ / std::sqrt(variance_) < 4)
+    if(mean_ * mean_ / variance_ < 16)
     {
         throw std::range_error("NormalRandomVariable: Variance of denominator is too large to allow approximation of division operator");
     }
 
-    double mean = mean_ / (std::pow(mean_, 2) - variance_);
-    double variance = variance_ / (std::pow(mean_, 4) - 2 * std::pow(mean_, 2) * variance_ + std::pow(variance_, 2));
+    double mean_squared = mean_ * mean_;
+    double mean = mean_ / (mean_squared - variance_);
+    double variance = variance_ / (mean_squared * mean_squared - 2 * mean_squared * variance_ + variance_ * variance_);
 
     return NormalRandomVariable(mean, variance);
 }
@@ -87,15 +88,16 @@ NormalRandomVariable operator/(double num, const NormalRandomVariable& rv)
 
 NormalRandomVariable operator/(const NormalRandomVariable& rv1, const NormalRandomVariable& rv2)
 {
-    double a = rv1.mean() / std::sqrt(rv1.variance());
-    double b = rv2.mean() / std::sqrt(rv2.variance());
+    double a = rv1.mean() * rv1.mean() / rv1.variance();
+    double b = rv2.mean() * rv2.mean() / rv2.variance();
 
     // Check that the conditions for the approximation are met
-    if(a < 2.5 && b >= 4)
+    if(a < 6.25 && b >= 16)
     {
         double r = rv2.variance() / rv1.variance();
-        double mean = a / (std::sqrt(r) * (1.01 * b - 0.2713));
-        double variance = (std::pow(a, 2) + 1) / (r * (std::pow(b, 2) + 0.108 * b - 3.795)) - std::pow(mean, 2);
+        double sqrt_b = std::sqrt(b);
+        double mean = std::sqrt(a) / (std::sqrt(r) * (1.01 * sqrt_b - 0.2713));
+        double variance = (a + 1) / (r * (b + 0.108 * sqrt_b - 3.795)) - mean * mean;
 
         return NormalRandomVariable(mean, variance);
     }
@@ -119,8 +121,8 @@ NormalRandomVariable operator*(double num, const NormalRandomVariable& rv)
 NormalRandomVariable operator*(const NormalRandomVariable& rv1, const NormalRandomVariable& rv2)
 {
     double mean = rv1.mean() * rv2.mean();
-    double delta1 = std::pow(rv1.mean(), 2) / rv1.variance();
-    double delta2 = std::pow(rv2.mean(), 2) / rv2.variance();
+    double delta1 = rv1.mean() * rv1.mean() / rv1.variance();
+    double delta2 = rv2.mean() * rv2.mean() / rv2.variance();
     double variance = rv1.variance() * rv2.variance() * (1 + delta1 + delta2);
 
     return NormalRandomVariable(mean, variance);
