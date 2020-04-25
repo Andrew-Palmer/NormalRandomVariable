ca255cc618a91315998ac2ddf440b70cd56e89f5
Andrew Palmer <palmer.a.w@gmail.com>
2020-04-25 17:37:04 +0200

    Fixed up usage example

--- example/example.cpp
+++ example/example.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
+
 #include "NormalRandomVariable.h"
 
 int main(int argc, char *argv[])
 {
-    NRV::NormalRandomVariable var(1,2);
+    NRV::NormalRandomVariable var(1,4);
+
+    std::cout << "Created a normally distributed random variable with mean of " << var.mean() << " and variance of " << var.variance() <<std::endl;
 }
